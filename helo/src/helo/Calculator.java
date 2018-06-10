7db7068683aaa8e34597f918861e7aaf78251af9
Nilesh Pawar <Nilesh Pawar@192.168.0.5>
2018-06-10 19:36:47 +0530

    TL--Calculator class and give report

---
+++ helo/src/helo/Calculator.java
@@ -0,0 +1,21 @@
+package helo;
+
+public class Calculator {
+
+	
+	static int sum(int a,int b){
+		return a+b;
+	}
+	
+	static int sub(int a,int b){
+		return a-b;
+	}
+	
+	static int mul(int a,int b){
+		return a*b;
+	}
+	
+	static int div(int a,int b){
+		return a/b;
+	}
+}
--- helo/src/helo/RoomProperties.java
+++ helo/src/helo/RoomProperties.java
@@ -4,7 +4,7 @@ public class RoomProperties {
 
 	public static void main(String[] args) {
 		//siddhesh--make class with name RoomMate with no of roommates,every  room mate name, age,education, native place, mob no. 
-
-	}
+		//how much time required guy's...
+		}
 
 }
