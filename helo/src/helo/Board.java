910f8765efb63b2a3013f020d8df6ec313751d09
Gopi <Gopi@192.168.0.4>
2018-06-10 07:09:53 -0700

    changes by balaji

---
+++ helo/src/helo/Board.java
@@ -0,0 +1,18 @@
+package helo;
+
+class SwitchBoard {
+
+	
+	static String boardName = "Anchore";
+	static int numOfButton = 7;
+	static int regularot = 1;
+	static int socket = 2; 
+	
+	static void board(){
+		System.out.println("boardName : "+boardName);
+		System.out.println("num of button : "+numOfButton);
+		System.out.println("regulator : "+regularot);
+		System.out.println("socket : "+socket);
+	}
+	
+}
--- helo/src/helo/RoomProperties.java
+++ helo/src/helo/RoomProperties.java
@@ -5,6 +5,7 @@ public class RoomProperties {
 	public static void main(String[] args) {
 		//siddhesh--make class with name RoomMate with no of roommates,every  room mate name, age,education, native place, mob no. 
 
+		SwitchBoard.board();
 	}
 
 }
